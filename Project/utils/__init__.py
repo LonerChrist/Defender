059a58a869cee550e6b21d527cf1e65f74be7a79
LonerChrist <awfullonelyman@gmail.com>
2026-05-01 08:55:06 +0500

    Delete Project directory

--- Project/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .security import require_api_key
-from .license import load_license, validate_license, generate_license_key
-
-__all__ = [
-    'require_api_key',
-    'load_license',
-    'validate_license',
-    'generate_license_key'
-]
